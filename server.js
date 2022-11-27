75a8bd1ecbf07f1ffae9cc8da5d5e90a30abe45a
Brent Keller <bikeller104@gmail.com>
2022-11-27 11:40:32 -0700

    Index.js has the content of index.html when the notes.html page is retrieved

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const PORT = 3001;
 //middleware to server static files from the public directory
 //since this comes before public/notes and public/index
 //this will probably get called instead of those other lines
-app.use(express.static('public'));
 
+app.use(express.static(__dirname + 'public'));
 
 // serve the html pages when the user requests them
 app.get('/public/notes', (req, res) => res.sendFile(path.join(__dirname,'/public/notes.html')));
+
 
 //this is the catch all path so if the user inputs any path not caught by the other path handlers
 //it will by default serve up the index.html page.
