926016e3e4af3ca8cb3f7e8b01d793bba63dc99f
Brent Keller <bikeller104@gmail.com>
2023-01-26 12:25:18 -0700

    added code to get heroku port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = require('./routes');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 
 //middleware to server static files from the public directory
